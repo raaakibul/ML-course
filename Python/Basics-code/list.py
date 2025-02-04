093acac495eebe211ebe2100e14cc662dc991194
rakibul-islam <raakibul.uap@gmail.com>
2025-02-04 20:31:53 +0600

    list code added

--- Python/Basics-code/list.py
+++ Python/Basics-code/list.py
@@ -11,3 +11,15 @@ print(even_numbers)
 matrix = [[1, 2], [3, 4], [5, 6]]
 flattened = [x for row in matrix for x in row]
 print(flattened)
+
+item1 = 'apple'
+item2 = 'banana'
+item3 = 'orange'
+print(item1 + ' ' + item2 + ' ' + item3) # apple banana orange
+
+# List example
+fruits = ['apple', 'banana', 'orange']
+print(fruits[0]) # apple
+print(fruits[1]) # banana
+print(fruits[2]) # orange
+print(fruits) # ['apple', 'banana', 'orange']
