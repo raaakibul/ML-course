9426b67050495a52fbd28d8c1208999f427c2587
rakibul-islam <raakibul.uap@gmail.com>
2025-02-04 20:52:41 +0600

    for loop code added

---
+++ Python/Basics-code/for.py
@@ -0,0 +1,13 @@
+monthly_expense = [2300, 2350, 2600, 2130, 2190]
+
+total = 0
+for item in monthly_expense:
+    total += item
+print("Total expenses: ", total)
+
+yearly_expense = [2500, 2600, 2400, 2800, 2900, 2750, 3000, 2900, 3100, 2600, 3000, 3250]
+
+total_yearly_expense = 0
+for item in yearly_expense:
+    total_yearly_expense += item
+print("Total yearly expense: ", total_yearly_expense)
