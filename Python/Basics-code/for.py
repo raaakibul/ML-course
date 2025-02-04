b87aab3236a986f3f0a0e9fd365dcf8c573077c4
rakibul-islam <raakibul.uap@gmail.com>
2025-02-04 20:56:10 +0600

    for loop code added

--- Python/Basics-code/for.py
+++ Python/Basics-code/for.py
@@ -11,3 +11,16 @@ total_yearly_expense = 0
 for item in yearly_expense:
     total_yearly_expense += item
 print("Total yearly expense: ", total_yearly_expense)
+
+# range function
+# range gives you range of numbers from 0 to n-1
+for i in range (0, 10):
+    print(i) # prints 0 to 9
+    
+for i in range (1, 10):
+    print(i*i) 
+    
+for i in range (len(monthly_expense)):
+    print('Month:', i+1, 'Expense:', monthly_expense[i])
+    total += monthly_expense[i]
+print('Total expense:', total)
