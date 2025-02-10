e434918c6ca940d3c142494ffc9fc43bf613327d
rakibul-islam <raakibul.uap@gmail.com>
2025-02-10 16:27:31 +0600

    matplotlib plot data

--- Matplotlib/plot.py
+++ Matplotlib/plot.py
@@ -1,5 +1,8 @@
 from matplotlib import pyplot as plt
 
+plt.style.use('ggplot')
+# plt.xkcd()
+
 dev_x = [25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
@@ -16,4 +19,7 @@ plt.xlabel("Ages")
 plt.ylabel("Median Salary (USD)")
 plt.title("Median Salary (USD) by Age")
 plt.legend()
+
+plt.tight_layout()
+plt.savefig('plot.png')
 plt.show()
