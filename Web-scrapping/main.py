2c6d47b776d7d63ccedb97d0e61f816a38029a56
rakibul-islam <raakibul.uap@gmail.com>
2025-02-18 23:26:33 +0600

    ML algorithm code added

--- Web-scrapping/main.py
+++ Web-scrapping/main.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://www.coursera.org/'
 r = requests.get(url)
 # print(r.status_code)
-print(r.text)
+# print(r.text)
+# print(r.content)
+# soup = BeautifulSoup(r.text, "lxml")
+# print(soup)
