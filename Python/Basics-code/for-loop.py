722a340516e1e27f0d491ab5279fad1167c17835
rakibul-islam <raakibul.uap@gmail.com>
2025-02-20 21:21:22 +0600

    break and continiue code

--- Python/Basics-code/for-loop.py
+++ Python/Basics-code/for-loop.py
@@ -6,3 +6,22 @@ for i in range(len(exp)):
     total = total + exp[i]
 
 print("Total Expenses :", total)
+
+# break
+key_location = "chair"
+locations = ['garage', 'living room', 'chair', 'closet']
+
+for i in locations:
+    if i ==key_location:
+        print("Key is found in", i)
+        break
+    else:
+        print("Key is not found in ", i)
+
+
+# Continue
+for i in range(1,6):
+    if i%2==0:
+        continue
+    print(i*i)
+    
