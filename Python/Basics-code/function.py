0b06e38bc589ae7806b6ba7df1f7aee74ef9fea4
rakibul-islam <raakibul.uap@gmail.com>
2025-02-20 21:36:18 +0600

    function code

--- Python/Basics-code/function.py
+++ Python/Basics-code/function.py
@@ -1,18 +1,18 @@
-# tom_exp_list = [2100,3400,3500]
-# joe_exp_list = [200,500,700]
+tom_exp_list = [2100,3400,3500]
+joe_exp_list = [200,500,700]
 
-# total = 0
-# for item in tom_exp_list:
-#     total = total + item
-# print("Tom's total expenses: ", total)
+total = 0
+for item in tom_exp_list:
+    total = total + item
+print("Tom's total expenses: ", total)
 
-# total = 0
-# for item in joe_exp_list:
-#     total = total + item
-# print("Joe's total expenses: ", total)
+total = 0
+for item in joe_exp_list:
+    total = total + item
+print("Joe's total expenses: ", total)
 
 # now using function
-
+print("Using Function:")
 
 def calculate_total(exp):
     total = 0
