a9d784bb96387c1666d708eb29a23c004d0d250f
rakibul-islam <raakibul.uap@gmail.com>
2025-02-23 23:27:58 +0600

    Exception handling

--- Python/Basics-code/exception-handling.py
+++ Python/Basics-code/exception-handling.py
@@ -3,8 +3,11 @@ x = input("Enter number1:")
 y = input("Enter number2:")
 try:
     z = int(x)/ int(y)
-except Exception as e:
+except ZeroDivisionError as e:
     print("Exception occured: ", e)
     z = None
-    
+except TypeError as e:
+    print("Type error exception:")
+    z = None
+
 print("Division is: ",z)
