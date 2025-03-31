f3d412291626c1b494508cb7ae921e66eda78bb9
rakibul-islam <raakibul.uap@gmail.com>
2025-03-31 20:20:26 +0600

    list comprehension and tuple code added

---
+++ Python/Data-structure/Tuple-operation.py
@@ -0,0 +1,9 @@
+t1 = (1, 2, 3)
+t2 = (4, 5)
+
+print(t1 + t2)   # Output: (1, 2, 3, 4, 5)
+print(t1 * 2)    # Output: (1, 2, 3, 1, 2, 3)
+print(len(t1))    # Output: 3
+
+print(2 in t1)  # Output: True
+print(10 in t1) # Output: False
---
+++ Python/Data-structure/list-comprehension.py
@@ -0,0 +1,5 @@
+squares = [x**2 for x in range(1, 6)]
+print(squares)  # Output: [1, 4, 9, 16, 25]
+
+evens = [x for x in range(10) if x % 2 == 0]
+print(evens)  # Output: [0, 2, 4, 6, 8]
