ed6cd37be9f8873a3928274b7aeaa20fa32746df
rakibul-islam <raakibul.uap@gmail.com>
2025-03-31 20:17:11 +0600

    dictionaries code added

---
+++ Python/Data-structure/dictionaries.py
@@ -0,0 +1,12 @@
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+print(my_dict)
+print(my_dict["name"])  # Output: Alice
+
+my_dict["age"] = 30  # Modify value
+print(my_dict)
+
+my_dict["country"] = "USA"
+print(my_dict)
+
+del my_dict["city"]
+print(my_dict)
