3de2ea98546f010c4190c6422a4a09afbb8b9ee3
rakibul-islam <raakibul.uap@gmail.com>
2025-03-31 20:21:52 +0600

    set

---
+++ Python/Data-structure/Set-advanced.py
@@ -0,0 +1,11 @@
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(A - B)  # Output: {1, 2} (Elements in A but not in B)
+print(B - A)  # Output: {5, 6} (Elements in B but not in A)
+print(A ^ B)  # Output: {1, 2, 5, 6} (Elements in A or B, but not both)
+print(A | B)  # Output: {1, 2, 3, 4, 5, 6} (Elements in A or B)
+print(A & B)  # Output: {3, 4} (Elements in both A and B)
+print(A <= B) # Output: False (A is a subset of B)
+print(A >= B) # Output: False (A is a superset of B)
+print(A < B)  # Output: False (A is a proper subset of B)
