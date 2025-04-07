a8c7a8a77ab8513eef124171f60f7cc340364a98
rakibul-islam <raakibul.uap@gmail.com>
2025-04-07 17:46:48 +0600

    instance method and variale for constructor

--- Python/OOP/constructor.py
+++ Python/OOP/constructor.py
@@ -1,11 +1,18 @@
 # class object constructor attribute methods()
 
 class Student:
-    def __init__(self):
-        # self.name = name
-        print("Constructor called")
-        print(self)
-
-s1= Student()
-s2 = Student()
-# print(s1)
+    def __init__(self, name, id):
+        self.name = name # instance variable
+        self.id = id     # instance variable
+        # print(f"Student Information: {self.name}, ID: {self.id}")
+    def details(self):
+        print("Name:", self.name, "ID:", self.id)
+s1= Student("Bob", 101)
+s2 = Student("Alice", 102)
+# print(s1.name)
+s1.details()
+s2.details()
+# print(s1.id)
+# print(s2.id)
+# print(s1.name, s1.id)
+# print(s2.name, s2.id)
