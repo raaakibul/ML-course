5901ef385f1dab8f8058ad883f6673851b505157
rakibul-islam <raakibul.uap@gmail.com>
2025-04-10 14:26:33 +0600

    file and folder organised

---
+++ Python/Basics-code/Module-1/python.py
@@ -0,0 +1,17 @@
+text = 'Python'
+print(text)
+# concatenation
+new_text = text + ' Programming'
+print(new_text)
+
+# slicing
+substring = text[1:4]
+print(new_text, substring)
+
+# list
+ages = [19,26,29]
+print(ages)
+
+languages = ['Python', 'Java', 'C++']
+print(languages[0])
+print(languages[-1])
